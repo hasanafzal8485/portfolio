d9c69123a91bc2b9f61be3bdfc1c88bab267ed33
Hasan Afzal <110741557+hasan-devpreneur@users.noreply.github.com>
2025-02-10 19:20:16 +0500

    Update social.js

--- data/social.js
+++ data/social.js
@@ -1,4 +1,4 @@
 export const social = {
-    github : 'https://github.com/hasanafzal8485',
-    linkedin : 'https://www.linkedin.com/in/hasan-afzal-full-stack-developer/',
+    github : 'https://github.com/hasan-devpreneur',
+    linkedin : 'https://www.linkedin.com/in/hasan-devpreneur',
 }
